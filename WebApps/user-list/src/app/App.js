7adffaa586ece76b66aab0008f0e402da8d65944
Nikola Alic <nikola@spesana.com>
2021-08-17 19:56:52 +0200

    From data to fetch

--- WebApps/user-list/src/app/App.js
+++ WebApps/user-list/src/app/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import { UserCards } from './user/UserCards';
 import Header from "./partials/Header"
 import Footer from "./partials/Footer"
-import { getUsers } from '../services/user.service';
+import { User } from '../entities/user';
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:[]
+    }
+  }
 
-const usersData = getUsers();
-const App = (props) => {
-  return (
-    <Fragment>
-      <Header title="React Users" />
-      <UserCards data={ usersData } />
-      <Footer />
-    </Fragment>
-  )
+  render() {
+    return (
+      <Fragment>
+        <Header title="React Users" />
+        <UserCards data={ this.state.data } />
+        <Footer />
+      </Fragment>
+    )
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=15').then(response=> {
+      return response.json();
+    }).then(response=> {
+      let userData = response.results.map(user=> new User(`${user.name.firs} ${user.name.last}`, user.email, user.dob, user.picture));
+      this.setState({data: userData});
+    });
+  }
 }
 
 export default App;
